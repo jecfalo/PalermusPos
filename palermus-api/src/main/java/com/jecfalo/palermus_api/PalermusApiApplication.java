42bc8575cfa3fc337f5fb2f46f8f622674687786
jecfalo <jeanfarialoor@hotmail.com>
2026-08-12 08:53:16 +0200

    Initial palermus pos dev

---
+++ palermus-api/src/main/java/com/jecfalo/palermus_api/PalermusApiApplication.java
@@ -0,0 +1,13 @@
+package com.jecfalo.palermus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalermusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalermusApiApplication.class, args);
+	}
+
+}
